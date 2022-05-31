c3ec8800568a35ab21587df9df015b846445c2d1
Vitaly Homonov <v.homonov@kasta.ua>
2022-05-31 14:47:25 +0300

    basic api routing setup

--- src/legomenon/core.clj
+++ src/legomenon/core.clj
@@ -1,6 +1,63 @@
-(ns legomenon.core)
+(ns legomenon.core
+  (:require [clojure.tools.logging :as log]
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+            [mount.core :as mount]
+            [aleph.http :as http]
+            [reitit.ring :as ring]
+            [ring.logger :as logger]))
+
+;; TODO: move to config
+(def PORT 5000)
+
+
+(defn handler [_req]
+  {:status 200
+   :body   "Hello World"})
+
+
+(defn bye-handler [_req]
+  {:status 200
+   :body "Bye"})
+
+
+(defn not-found [_]
+  {:status 404
+   :body "Not found"})
+
+
+(defn make-app []
+  (let [routes [["/hello" {:get {:handler handler}}]
+                ["/bye" {:get {:handler bye-handler}}]]]
+    (-> (ring/router routes)
+        (ring/ring-handler not-found)
+        (logger/wrap-with-logger))))
+
+
+(mount/defstate server
+  :start (let [server (http/start-server (make-app) {:port PORT})]
+           (log/info "Started server on" PORT)
+           server)
+  :stop (do
+          (log/info "Stopping server on" PORT)
+          (.close server)))
+
+
+(defn -main [& _args]
+  (mount/start #'server))
+
+
+;; development stuff
+
+(defn start []
+  (mount/start))
+
+
+(defn stop []
+  (mount/stop))
+
+
+(defn reload []
+  (stop)
+  (start))
+
+
