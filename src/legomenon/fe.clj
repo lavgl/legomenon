c8cf09589ea974ca9bdb9307610ea06e956b22b0
Vitaly Homonov <v.homonov@kasta.ua>
2022-06-08 15:57:56 +0300

    add bootstrap. add some basic responsive styling

--- src/legomenon/fe.clj
+++ src/legomenon/fe.clj
@@ -4,21 +4,35 @@
             [legomenon.db :as db]))
 
 
-(defn page [content]
+(defn page [header & content]
   (hiccup.page/html5 {:encoding "UTF-8"}
     [:head {}
      [:meta {:charset "UTF-8"}]
-     (hiccup.page/include-css "/public/css/main.css")]
+     [:meta {:name    "viewport"
+             :content "width=device-width, initial-scale=1"}]
+     (hiccup.page/include-css
+       ;; "/public/css/normalize.css"
+       "/public/css/bootstrap.min.css"
+       "/public/css/main.css"
+       )]
     [:body
-     [:div.content
+     (or header nil)
+     [:div.container
       content]
-     (hiccup.page/include-js "/public/js/htmx.min.js")
+     (hiccup.page/include-js
+       "/public/js/htmx.min.js"
+       "/public/js/bootstrap.bundle.min.js")
      [:script
       "
 htmx.onLoad(element => {
   if (element.tagName == 'TR' && element.nextSibling.tagName == 'TR') {
     element.nextSibling.focus();
   }});"]]))
+
+
+;; (defn navbar []
+;;   [:nav ]
+;;   )
 
 
 ;; books list frontend
@@ -28,7 +42,7 @@ htmx.onLoad(element => {
   [:form {:method  "post"
           :enctype "multipart/form-data"
           :action  "/api/books/add"}
-   [:label {:for "file"} "Choose Book to upload"]
+   ;; [:label {:for "file"} "Choose Book to upload"]
    [:input {:type "file" :id "file" :name "file"}]
    [:button "Submit"]])
 
@@ -52,10 +66,13 @@ htmx.onLoad(element => {
 
 (defn index [& _]
   {:status 200
-   :body   (page [:div
-                  [:h1 "Hey, this is the header"]
-                  (add-book-form)
-                  (books-list)])})
+   :body   (page nil
+             [:div
+              [:h4 "Add new book:"]
+              (add-book-form)
+              [:hr]
+              [:h4 "My books:"]
+              (books-list)])})
 
 
 ;; book dictionary page frontend
@@ -87,55 +104,64 @@ htmx.onLoad(element => {
 
 
 (defn render-valuable-row [{:keys [lemma count id]}]
-  [:tr {:tabindex   "0"
-        :hx-trigger "keyup[key=='k' || key == 't']"
-        :hx-post    "/api/words/op/"
-        :hx-vals    (format "js:{key: event.key, id: '%s'}" id)
-        :hx-swap    "outerHTML"}
+  [:tr.dict-word
+   {:tabindex   "0"
+    :hx-trigger "keyup[key=='k' || key == 't']"
+    :hx-post    "/api/words/op/"
+    :hx-vals    (format "js:{key: event.key, id: '%s'}" id)
+    :hx-swap    "outerHTML"}
    [:td lemma]
    [:td count]])
 
 
 (defn render-trash-row [{:keys [lemma count id]}]
-  [:tr.trash {:tabindex   "0"
-              :hx-trigger "keyup[key == 'u']"
-              :hx-post    "/api/words/op/"
-              :hx-vals    (format "js:{key: event.key, id: '%s'}" id)
-              :hx-swap    "outerHTML"}
+  [:tr.trash.dict-word
+   {:tabindex   "0"
+    :hx-trigger "keyup[key == 'u']"
+    :hx-post    "/api/words/op/"
+    :hx-vals    (format "js:{key: event.key, id: '%s'}" id)
+    :hx-swap    "outerHTML"}
    [:td lemma]
    [:td count]])
 
 
 (defn render-known-row [{:keys [lemma count id]}]
-  [:tr.known {:tabindex   "0"
-              :hx-trigger "keyup[key == 'u']"
-              :hx-post    "/api/words/op/"
-              :hx-vals    (format "js:{key: event.key, id: '%s'}" id)
-              :hx-swap    "outerHTML"}
+  [:tr.known.dict-word
+   {:tabindex   "0"
+    :hx-trigger "keyup[key == 'u']"
+    :hx-post    "/api/words/op/"
+    :hx-vals    (format "js:{key: event.key, id: '%s'}" id)
+    :hx-swap    "outerHTML"}
    [:td lemma]
    [:td count]])
 
 
 (defn words-table [book-id]
   (let [words (db/q db/conn (book-words-q book-id))]
-    [:table
-     [:tr [:th "Word"] [:th "Count"]]
-     (map (fn [word]
-            (cond
-              (pos? (:is_known word))
-              (render-known-row word)
+    [:div.row
+     [:div.col]
+     [:div.col-sm-9
+      [:table
+       [:thead
+        [:tr.dict-word [:th "Word"] [:th "Count"]]]
+       [:tbody
+        (map (fn [word]
+               (cond
+                 (pos? (:is_known word))
+                 (render-known-row word)
 
-              (pos? (:is_trash word))
-              (render-trash-row word)
+                 (pos? (:is_trash word))
+                 (render-trash-row word)
 
-              :else
-              (render-valuable-row word)))
-       words)]))
+                 :else
+                 (render-valuable-row word)))
+          words)]]]
+     [:div.col]]))
 
 
 (defn book-title-q [book-id]
   {:from   [:books]
-   :select [[:user_entered_title :title]
+   :select [[[:coalesce :user_entered_title :filename] :title]
             [true :is_book_exists]]
    :where  [:= :id book-id]})
 
@@ -150,10 +176,12 @@ htmx.onLoad(element => {
                 [:button {:type "submit"} "OK"]])}))
 
 
-(defn book-title [{:keys [book-id title]}]
-  [:h1 {:hx-get  (format "/fragments/edit-book-title/?book-id=%s&title=%s" book-id title)
-        :hx-swap "outerHTML"}
-   (or title "click to enter title")])
+(defn book-title [{:keys [_book-id title]}]
+  [:div.row
+   [:div.col]
+   [:div.col-sm-9
+    [:h1 (or title "click to enter title")]]
+   [:div.col]])
 
 
 (defn book-dictionary-page [req]
@@ -161,11 +189,10 @@ htmx.onLoad(element => {
         {:keys [title is_book_exists]} (db/one db/conn (book-title-q book-id))]
     (if (pos? is_book_exists)
       {:status 200
-       :body   (html (page
-                       [:div
-                        (book-title {:book-id book-id
-                                     :title   title})
-                        (words-table book-id)]))}
+       :body   (html (page nil
+                       (book-title {:book-id book-id
+                                    :title   title})
+                       (words-table book-id)))}
       {:status 404
        :body   "not found"})))
 
@@ -179,7 +206,7 @@ htmx.onLoad(element => {
                                      :where  [:and
                                               [:= :id book-id]
                                               [:= :deleted_at nil]]}))]
-    [:div.book-text {} text]))
+    [:div.container.book-text {} text]))
 
 
 (defn book-text-page [req]
@@ -187,7 +214,7 @@ htmx.onLoad(element => {
         {:keys [title is_book_exists]} (db/one db/conn (book-title-q book-id))]
     (if (pos? is_book_exists)
       {:status 200
-       :body   (html (page
+       :body   (html (page nil
                        [:div
                         (book-title {:book-id book-id
                                      :title   title})
