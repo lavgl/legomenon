b274bf1b212927d0d68184ad16910d3e5d1285e7
Vitaly Homonov <avg.idm@gmail.com>
2022-08-07 20:16:05 +0300

    small cleanup: extract words-sorting-subq

--- src/legomenon/words/dal.clj
+++ src/legomenon/words/dal.clj
@@ -2,23 +2,26 @@
   (:require [legomenon.db :as db]))
 
 
+(def words-sorting-subq
+  [:case
+   [:= :list "memo"]      4
+   [:= :list nil]         3
+   [:= :list "postponed"] 2
+   [:= :list "known"]     1
+   [:= :list "trash"]     0])
+
+
 (defn book-words-q [book-id]
-  (let [priority [:case
-                  [:= :list "memo"]      4
-                  [:= :list nil]         3
-                  [:= :list "postponed"] 2
-                  [:= :list "known"]     1
-                  [:= :list "trash"]     0]]
-    {:from      [:lemma_count]
-     :left-join [:my_words [:and
-                            [:= :my_words.word :lemma_count.lemma]
-                            [:= :my_words.deleted_at nil]]]
-     :select    [:lemma :count :id :my_words.list]
-     :where     [:and
-                 [:= :lemma_count.book_id book-id]
-                 [:> :lemma_count.count 1]]
-     :order-by  [[priority :desc]
-                 [:count :desc]]}))
+  {:from      [:lemma_count]
+   :left-join [:my_words [:and
+                          [:= :my_words.word :lemma_count.lemma]
+                          [:= :my_words.deleted_at nil]]]
+   :select    [:lemma :count :id :my_words.list]
+   :where     [:and
+               [:= :lemma_count.book_id book-id]
+               [:> :lemma_count.count 1]]
+   :order-by  [[words-sorting-subq :desc]
+               [:count :desc]]})
 
 
 (defn words-list-by-book-id [book-id]
