a3dc22f8ce405bd33ae7b2d31d762a24e22fe132
Vitaly Homonov <avg.idm@gmail.com>
2022-09-17 16:19:59 +0300

    fix bootstrap js link

--- src/legomenon/fragments.clj
+++ src/legomenon/fragments.clj
@@ -29,7 +29,7 @@
                :crossorigin "anonymous"}]
 
      ;; bootstrap
-     [:script {:src         "https://unpkg.com/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+     [:script {:src         "https://unpkg.com/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
                :integrity   "sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
                :crossorigin "anonymous"}]
 
